b44757a514fa8be33eedce535168317b019ccefb
weiqiang <851377470@qq.com>
2017-09-28 11:09:09 +0800

    添加

---
+++ pages/home/home.js
@@ -0,0 +1,92 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    loadingHidden: false, // loading
+    storeInfo: {
+      name:"蜗牛店铺",
+      introduce:"很好吃很啊华东师范很好吃很啊华东师范",
+      photo:"https://img.meituan.net/msmerchant/b60ca34725098c2510d9942ae34675ae417400.jpg@750w_320h_1e_1c",
+      expire:'201',
+      qr:"../../images/qr.png"
+    },
+    todayInfo:{
+      order:100,
+      turnover:5000
+    },
+    swiperCurrent: 0,
+    selectCurrent: 0
+  },
+
+  //事件处理函数
+  swiperchange: function (e) {
+    //console.log(e.detail.current)
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  toDetailsTap: function (e) {
+    wx.navigateTo({
+      url: "/pages/goods-details/index?id=" + e.currentTarget.dataset.id
+    })
+  },
+  tapBanner: function (e) {
+    if (e.currentTarget.dataset.id != 0) {
+      wx.navigateTo({
+        url: "/pages/goods-details/index?id=" + e.currentTarget.dataset.id
+      })
+    }
+  },
+  bindTypeTap: function (e) {
+    this.setData({
+      selectCurrent: e.index
+    })
+  },
+  onLoad: function () {
+    var that = this
+    /*
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+    */
+   /* wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/banner/list',
+      data: {
+        key: 'mallName'
+      },
+      success: function (res) {
+        if (res.data.code == 404) {
+          wx.showModal({
+            title: '提示',
+            content: '请在后台添加 banner 轮播图片',
+            showCancel: false
+          })
+        } else {
+          that.setData({
+            banners: res.data.data
+          });
+        }
+      }
+    })
+
+  //https://www.wendin.cn/dcb/wxwithdrawRecord.do?apply&sessionId=43892
+
+    wx.request({
+      url: 'https://www.wendin.cn/dcb/wxwithdrawRecord.do?apply&sessionId=43892',
+      success: function (res) {
+        console.log(res)
+      }
+    })
+    */
+
+  }
+})
---
+++ pages/today-order/index.js
@@ -0,0 +1,64 @@
+var wxpay = require('../../utils/pay.js')
+var app = getApp()
+Page({
+  data: {
+    statusType: ["已完成", "已关闭", "未完成"],
+    currentType: 0,
+    tabClass: ["", "", "", "", ""],
+    orderList: null
+  },
+  statusTap: function (e) {
+    var curType = e.currentTarget.dataset.index;
+    this.data.currentType = curType
+    this.setData({
+      currentType: curType
+    });
+    this.onShow();
+  },
+  orderDetail: function (e) {
+    var orderId = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: "/pages/order-details/index?id=" + orderId
+    })
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+
+  },
+  onReady: function () {
+    // 生命周期函数--监听页面初次渲染完成
+
+  },
+  onShow: function () {
+    // 获取订单列表
+
+    this.setData({
+      orderList: [
+        { "dateAdd": "2018-9-9", "statusStr": "已完成", "status": -1, "orderNumber": "FF1245454", "customer": "魏强", "pic": "https://img.meituan.net/msmerchant/b60ca34725098c2510d9942ae34675ae417400.jpg@750w_320h_1e_1c","tel":"18628977163" },
+        { "dateAdd": "2018-9-9", "statusStr": "已关闭", "status": 1, "orderNumber": "FF1245454", "customer": "魏强", "pic": "http://p1.meituan.net/deal/e2f4eb1c2edd2fc2bc80783d4eeb42cc94206.jpg@180w_164h_1e_1c", "tel": "18628977163" }
+      ]
+    });
+
+  },
+  contact:function(e){
+    wx.makePhoneCall({
+      phoneNumber: e.target.dataset.tel //仅为示例，并非真实的电话号码
+    })
+  },
+  onHide: function () {
+    // 生命周期函数--监听页面隐藏
+
+  },
+  onUnload: function () {
+    // 生命周期函数--监听页面卸载
+
+  },
+  onPullDownRefresh: function () {
+    // 页面相关事件处理函数--监听用户下拉动作
+
+  },
+  onReachBottom: function () {
+    // 页面上拉触底事件的处理函数
+
+  }
+})
